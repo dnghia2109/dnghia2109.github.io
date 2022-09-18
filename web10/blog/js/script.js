643d45710f9bae220a383b1b32cea23074757d82
Lai Duy Nghia <laiduynghia21@gmail.com>
2022-09-18 15:45:50 +0700

    update 18092022

--- web10/blog/js/script.js
+++ web10/blog/js/script.js
@@ -1,8 +1,8 @@
-
 $(document).ready(function(){
     $('.multiple-items').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        
     });
   });
